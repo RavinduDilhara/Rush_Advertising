cb7ad8cb45d82d787191cd90a0d8af964da0edb8
RavinduDilhara <ravindu.1dilhara@gmail.com>
2020-03-19 21:54:19 +0530

    admin_login

--- app/src/main/java/com/example/mad/Check.java
+++ app/src/main/java/com/example/mad/Check.java
@@ -2,13 +2,28 @@ package com.example.mad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Check extends AppCompatActivity {
+
+    ImageView admin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check);
+
+        admin = findViewById(R.id.imageButtonCheckAdmin);
+
+        admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Check.this, Admin_Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
