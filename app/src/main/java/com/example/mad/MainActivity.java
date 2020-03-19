be3ec927690edff6cf8ade2947e179da0ab622a8
RavinduDilhara <ravindu.1dilhara@gmail.com>
2020-03-19 21:28:12 +0530

    check, toolbar

--- app/src/main/java/com/example/mad/MainActivity.java
+++ app/src/main/java/com/example/mad/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.mad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView3);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Check.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
